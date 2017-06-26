754afb52e6ad72ab38073e3fb1d4b564454a03e1
Andrés Sarmiento <Sarmiento@amatea.org>
2017-06-26 15:24:51 -0500

    axios

--- firstapp/src/components/Faq/Faq.js
+++ firstapp/src/components/Faq/Faq.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 
 class Faq extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      faqs: []
+    }
+  } 
+    componentDidMount() {
+      axios.get('http://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+          const faqs = res.data.slice(0,10);
+          this.setState({ faqs })
+        })
+    }
+
   render() {
     return (
       <div>
-        Faq
+        <div className="columns">
+         {this.state.faqs.map(faq =>
+           <div className="column is-one-third">
+            <div className="card">
+            <div clasName="card-content">
+              <p className="title">{ faq.title }</p>
+              <p className="answer">{ faq.body }</p>
+            </div>
+            </div>
+           </div>
+          )}
+        </div>
       </div>
     );
   }
--- firstapp/src/components/Home/Home.js
+++ firstapp/src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
   render() {
+
+    let heading = "Soaring to new heights"
+    let subheading = "Lorem ipsum dolor sit amet, consectetur adipscing elit, sed"
+
     return (
       <div>
-        Home
+        <section className="Hero">
+          <div className="hero-body">
+            <div className="container">
+            <h1 className="title">{ heading }</h1>
+            <div className="is-two-thirds column is-paddinggless">
+              <h2 className="subtitle is-4">{subheading}</h2>
+            </div>
+              <a className="button is-large is-primary" id="learn">Learn more</a>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
